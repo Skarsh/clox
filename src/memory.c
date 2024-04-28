cf151756980f80b6edc2927bcb53c579ea8afb7e
Thomas Skarshaug <skarshaug.thomas@gmail.com>
2024-04-28 16:26:06 +0200

    Chapter 14 done, Chunks of Bytecode

---
+++ src/memory.c
@@ -0,0 +1,19 @@
+//
+// Created by skars on 4/28/2024.
+//
+
+#include <stdlib.h>
+
+#include "memory.h"
+
+
+void *reallocate(void *pointer, size_t oldSize, size_t newSize) {
+    if (newSize == 0) {
+        free(pointer);
+        return NULL;
+    }
+
+    void* result = realloc(pointer, newSize);
+    if (result == NULL) exit(1);
+    return result;
+}
