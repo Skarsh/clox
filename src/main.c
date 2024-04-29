3e7b49e21929611d1ebd0c3884dbc86e2c50493c
Thomas Skarshaug <skarshaug.thomas@gmail.com>
2024-04-29 20:04:05 +0200

    Chapter 15 done, A Virtual Machine

--- src/main.c
+++ src/main.c
@@ -2,9 +2,11 @@
 
 #include "chunk.h"
 #include "debug.h"
+#include "vm.h"
 
 int main(void)
 {
+    initVM();
     Chunk chunk;
     initChunk(&chunk);
 
@@ -12,9 +14,24 @@ int main(void)
     writeChunk(&chunk, OP_CONSTANT, 123);
     writeChunk(&chunk, constant, 123);
 
+    constant = addConstant(&chunk, 3.4);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    writeChunk(&chunk, OP_ADD, 123);
+
+    constant = addConstant(&chunk, 5.6);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    writeChunk(&chunk, OP_DIVIDE, 123);
+
+    writeChunk(&chunk, OP_NEGATE, 123);
     writeChunk(&chunk, OP_RETURN, 123);
 
     disassembleChunk(&chunk, "test chunk");
+    interpret(&chunk);
+    freeVM();
     freeChunk(&chunk);
     return 0;
 }
